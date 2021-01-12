23022f6a7dfd8f86a6faf38c0d632cb210d4b72c
Marcus Couto <markitovc@gmail.com>
2021-01-12 00:39:00 -0300

    include cache in listPokemons call

--- bff-api-graphql/src/resolvers/pokemon/query/listPokemons.js
+++ bff-api-graphql/src/resolvers/pokemon/query/listPokemons.js
@@ -1,23 +1,48 @@
+const redisClient = require('../../../config/redis')
 const formatPokemonType = require('../../../utils/formatPokemonType')
 const formatColorList = require('../../../utils/formatColorList')
 const getIdByUrl = require('../../../utils/getIdByURL')
 
+const getPokemonTypeInCache = async (dataSources, pokemonId) => {
+  const dataCache = await redisClient.get(`pokemon_${pokemonId}`)
+  if (dataCache) {
+    return JSON.parse(dataCache)
+  }
+  const result = await dataSources.pokeAPI.getPokemonType(pokemonId)
+  await redisClient.setex(
+    `pokemon_${pokemonId}`,
+    1440,
+    JSON.stringify(result)
+  )
+  return result
+}
+
+const getColorByTypeInCache = async (dataSources) => {
+  const dataCache = await redisClient.get('listColors')
+  if (dataCache) {
+    return JSON.parse(dataCache)
+  }
+  const response = await dataSources.colorAPI.listAllcolors()
+  await redisClient.setex('listColors', 1440, JSON.stringify(response))
+  return response
+}
+
 const listPokemonType = async (dataSources, name, url, colorList) => {
   const pokemonId = getIdByUrl(url)
-  const { types } = await dataSources.pokeAPI.getPokemonType(pokemonId)
-  const data = {
+  const result = await getPokemonTypeInCache(dataSources, pokemonId)
+
+  return {
     name,
-    types: formatPokemonType(types, colorList)
+    types: formatPokemonType(result.types, colorList)
   }
-  return data
 }
 
 const listPokemons = async (root, data, { dataSources }) => {
   const { offset, limit } = data.page
 
   const { results } = await dataSources.pokeAPI.getPokemonList(offset, limit)
-  const response = await dataSources.colorAPI.listAllcolors()
-  const colorList = await formatColorList(response)
+  const colorByType = await getColorByTypeInCache(dataSources)
+  const colorList = formatColorList(colorByType)
 
   const pokemonList = await Promise.all(
     results.map(async ({ name, url }) => {
